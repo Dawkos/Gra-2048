5f81bc4efc8dd63d32bc6273bc6789c3a6aebd8b
Dawid Koszczuk <xdawidx58@gmail.com>
2022-01-04 16:03:57 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ import javax.swing.*;
 
 public class Main extends JPanel {
     enum State {
-        start, wygrana, uruchomiona, koniec
+        start, won, running, over
     }
-
     final Color[] colorTable = {
             new Color(0x701710), new Color(0xFFE4C3), new Color(0xfff4d3),
             new Color(0xffdac3), new Color(0xe7b08e), new Color(0xe7bf8e),
@@ -21,7 +20,6 @@ public class Main extends JPanel {
     private Color startColor = new Color(0xFFEBCD);
     private Random rand = new Random();
     private Tile[][] tiles;
-    //rozmiar planszy
     private int side = 4;
     private State gamestate = State.start;
     private boolean checkingAvailableMoves;
@@ -43,23 +41,22 @@ public class Main extends JPanel {
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_UP:
-                        //moveUp();
+                        moveUp();
                         break;
                     case KeyEvent.VK_DOWN:
-                        //moveDown();
+                        moveDown();
                         break;
                     case KeyEvent.VK_LEFT:
-                        //moveLeft();
+                        moveLeft();
                         break;
                     case KeyEvent.VK_RIGHT:
-                        //moveRight();
+                        moveRight();
                         break;
                 }
                 repaint();
             }
         });
     }
-
     @Override
     public void paintComponent(Graphics gg) {
         super.paintComponent(gg);
@@ -68,29 +65,27 @@ public class Main extends JPanel {
                 RenderingHints.VALUE_ANTIALIAS_ON);
         drawGrid(g);
     }
-
     void startGame() {
-        if (gamestate != State.uruchomiona) {
+        if (gamestate != State.running) {
             score = 0;
             highest = 0;
-            gamestate = State.uruchomiona;
+            gamestate = State.running;
             tiles = new Tile[side][side];
-            //addRandomTile();
-            //addRandomTile();
+            addRandomTile();
+            addRandomTile();
         }
     }
-
     void drawGrid(Graphics2D g) {
         g.setColor(gridColor);
         g.fillRoundRect(200, 100, 499, 499, 15, 15);
-        if (gamestate == State.uruchomiona) {
+        if (gamestate == State.running) {
             for (int r = 0; r < side; r++) {
                 for (int c = 0; c < side; c++) {
                     if (tiles[r][c] == null) {
                         g.setColor(emptyColor);
                         g.fillRoundRect(215 + c * 121, 115 + r * 121, 106, 106, 7, 7);
                     } else {
-                        //drawTile(g, r, c);
+                        drawTile(g, r, c);
                     }
                 }
             }
@@ -101,14 +96,111 @@ public class Main extends JPanel {
             g.setFont(new Font("SansSerif", Font.BOLD, 128));
             g.drawString("2048", 310, 270);
             g.setFont(new Font("SansSerif", Font.BOLD, 20));
-            if (gamestate == State.wygrana) {
+            if (gamestate == State.won) {
                 g.drawString("Wygrałeś!", 390, 350);
-            } else if (gamestate == State.koniec)
+            } else if (gamestate == State.over)
                 g.drawString("Przegrałeś", 400, 350);
             g.setColor(gridColor);
-            g.drawString("Kliknij by zagrać", 330, 470);
-            g.drawString("(Używaj strzałek do sterowania)", 310, 530);
+            g.drawString("Kliknij aby zagrać", 330, 470);
+            g.drawString("(używaj strzałek do sterowania)", 310, 530);
         }
+    }
+    void drawTile(Graphics2D g, int r, int c) {
+        int value = tiles[r][c].getValue();
+        g.setColor(colorTable[(int) (Math.log(value) / Math.log(2)) + 1]);
+        g.fillRoundRect(215 + c * 121, 115 + r * 121, 106, 106, 7, 7);
+        String s = String.valueOf(value);
+        g.setColor(value < 128 ? colorTable[0] : colorTable[1]);
+        FontMetrics fm = g.getFontMetrics();
+        int asc = fm.getAscent();
+        int dec = fm.getDescent();
+        int x = 215 + c * 121 + (106 - fm.stringWidth(s)) / 2;
+        int y = 115 + r * 121 + (asc + (106 - (asc + dec)) / 2);
+        g.drawString(s, x, y);
+    }
+    private void addRandomTile() {
+        int pos = rand.nextInt(side * side);
+        int row, col;
+        do {
+            pos = (pos + 1) % (side * side);
+            row = pos / side;
+            col = pos % side;
+        } while (tiles[row][col] != null);
+        int val = rand.nextInt(10) == 0 ? 4 : 2;
+        tiles[row][col] = new Tile(val);
+    }
+    private boolean move(int countDownFrom, int yIncr, int xIncr) {
+        boolean moved = false;
+        for (int i = 0; i < side * side; i++) {
+            int j = Math.abs(countDownFrom - i);
+            int r = j / side;
+            int c = j % side;
+            if (tiles[r][c] == null)
+                continue;
+            int nextR = r + yIncr;
+            int nextC = c + xIncr;
+            while (nextR >= 0 && nextR < side && nextC >= 0 && nextC < side) {
+                Tile next = tiles[nextR][nextC];
+                Tile curr = tiles[r][c];
+                if (next == null) {
+                    if (checkingAvailableMoves)
+                        return true;
+                    tiles[nextR][nextC] = curr;
+                    tiles[r][c] = null;
+                    r = nextR;
+                    c = nextC;
+                    nextR += yIncr;
+                    nextC += xIncr;
+                    moved = true;
+                } else if (next.canMergeWith(curr)) {
+                    if (checkingAvailableMoves)
+                        return true;
+                    int value = next.mergeWith(curr);
+                    if (value > highest)
+                        highest = value;
+                    score += value;
+                    tiles[r][c] = null;
+                    moved = true;
+                    break;
+                } else
+                    break;
+            }
+        }
+        if (moved) {
+            if (highest < target) {
+                clearMerged();
+                addRandomTile();
+                if (!movesAvailable()) {
+                    gamestate = State.over;
+                }
+            } else if (highest == target)
+                gamestate = State.won;
+        }
+        return moved;
+    }
+    boolean moveUp() {
+        return move(0, -1, 0);
+    }
+    boolean moveDown() {
+        return move(side * side - 1, 1, 0);
+    }
+    boolean moveLeft() {
+        return move(0, 0, -1);
+    }
+    boolean moveRight() {
+        return move(side * side - 1, 0, 1);
+    }
+    void clearMerged() {
+        for (Tile[] row : tiles)
+            for (Tile tile : row)
+                if (tile != null)
+                    tile.setMerged(false);
+    }
+    boolean movesAvailable() {
+        checkingAvailableMoves = true;
+        boolean hasMoves = moveUp() || moveDown() || moveLeft() || moveRight();
+        checkingAvailableMoves = false;
+        return hasMoves;
     }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
